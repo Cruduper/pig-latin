6e941e0628e04cfefe392e06ed3ddf776df48ba2
Eric Crudup <briefcasemanx@gmail.com>
2022-01-20 01:21:16 -0800

    WIP: creating a pigLatin() function and isVowel() function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
+function isVowel(char)  {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let isVowel = false;
 
+  vowels.forEach(function(vowel)  {
+    if (char === vowel)
+      isVowel = true
+  });
+
+  return isVowel;
+
+
+}
+
+function pigLatin(text)  {
+
+  let prefix = "";
+  let suffix = "";
+
+  if  ( isVowel(text.charAt(0)) ) {
+    suffix = "way";
+  }
+  else  {
+    for ( currentChar = text.charAt(0); isVowel(0)
+    
+  }
+
+  return text + suffix;
+}
 
 
 
